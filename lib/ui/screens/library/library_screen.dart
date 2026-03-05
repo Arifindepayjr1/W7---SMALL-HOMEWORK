d37adf38328016353cec56834991fded1229992a
Arifin Depay Jr <121757305+Arifindepayjr1@users.noreply.github.com>
2026-03-05 14:00:46 +0700

    Add library

--- lib/ui/screens/library/library_screen.dart
+++ lib/ui/screens/library/library_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
- 
+import 'package:w7_small_homework/ui/screens/library/view_models/library_view_model.dart';
+import 'package:w7_small_homework/ui/screens/library/widgets/library_content.dart';
+
 import '../../../data/repositories/songs/song_repository.dart';
 import '../../../model/songs/song.dart';
 import '../../states/player_state.dart';
@@ -12,65 +14,16 @@ class LibraryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // 1- Read the globbal song repository
-    SongRepository songRepository = context.read<SongRepository>();
-    List<Song> songs = songRepository.fetchSongs();
-
-    // 2- Read the globbal settings state
-    AppSettingsState settingsState = context.read<AppSettingsState>();
-
-    // 3 - Watch the globbal player state
-    PlayerState playerState = context.watch<PlayerState>();
-
-    return Container(
-      color: settingsState.theme.backgroundColor,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(height: 16),
-          Text("Library", style: AppTextStyles.heading),
-
-          SizedBox(height: 50),
-
-          Expanded(
-            child: ListView.builder(
-              itemCount: songs.length,
-              itemBuilder: (context, index) => SongTile(
-                song: songs[index],
-                isPlaying: playerState.currentSong == songs[index],
-                onTap: () {
-                  playerState.start(songs[index]);
-                },
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class SongTile extends StatelessWidget {
-  const SongTile({
-    super.key,
-    required this.song,
-    required this.isPlaying,
-    required this.onTap,
-  });
-
-  final Song song;
-  final bool isPlaying;
-  final VoidCallback onTap;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      onTap: onTap,
-      title: Text(song.title),
-      trailing: Text(
-        isPlaying ? "Playing" : "",
-        style: TextStyle(color: Colors.amber),
-      ),
+    final PlayerState playerState = context.watch<PlayerState>();
+    final SongRepository songRepository = context.watch<SongRepository>();
+    return ChangeNotifierProvider(
+      child: LibraryContent(),
+      create: (_) {
+        return LibraryViewModel(
+          playerState: playerState,
+          songRepository: songRepository,
+        );
+      },
     );
   }
 }
